eadfddd8bb5b2bbd25d3e05d14460ea7ed947451
haydenanderl33 <hayden.anderl33@gmail.com>
2022-10-19 17:42:48 -0600

    routing through index.html

--- serverV2/app.js
+++ serverV2/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require('path')
 
 //Security Packages
 const helmet = require("helmet");
@@ -30,10 +31,22 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/build/index.html'), function (err) {
+    console.log('hello')
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 app.use("/api/v1/password", passwordRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/workouts", authenticate, workoutRouter);
 app.use("/api/v1/create-checkout-session", stripeRouter);
+
+
 
 const port = SERVER_PORT;
 
